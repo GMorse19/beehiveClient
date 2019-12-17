f726d4d2c5b4b339f72cbaeaa4933ac4c587e4ea
GMorse19 <malorse@comcast.net>
2019-12-17 09:37:44 -0500

    Changed heroku path

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://quiet-scrubland-10243.herokuapp.com/',
+  production: 'https://quiet-scrubland-10243.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
